8fd53e7aeade1b81b0ae26c34f97433915df1b21
KruZ73 <kruzyckikacper4@gmail.com>
2022-11-23 10:01:08 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+    require('class/User.class.php');
+    echo "<pre>";
+
+
+
+    $db = new mysqli('localhost', 'root', '', 'loginForm');
+    $user = new User("jkowalski","tajneHasło");
+    $user->login();
+
+
+
+    if($user->isAuth()) {
+        echo "Użytkownik zalogowany poprawnie";
+    } else {
+        echo "Błąd logowania";
+    }
+
+    
+?>
