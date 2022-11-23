c17776517736f8b0f4167a824d386d38615330e6
KruZ73 <kruzyckikacper4@gmail.com>
2022-11-23 11:02:31 +0100

    j

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $db = new mysqli('localhost', 'root', '', 'loginform');
 
     $loader = new \Twig\Loader\ArrayLoader([
-        'index' => "Hello {{name}}!"
+        'index' => "Hello {{name}}!",
     ]);
     $twig = new \Twig\Environment($loader);
 ?>
--- index.php
+++ index.php
@@ -3,7 +3,20 @@
     require_once('config.php');
     require_once('class/User.class.php');
 
-    $user = new User('jkowalski', 'tajneHasło');
+    echo $twig->render('index', ['name' => 'Paweł']);
+
+    //    $user = new User('jkowalski', 'tajneHasło');
+
+
+    /*
+
+        if($user->register()) {
+            echo "Zarejestrowano poprawnie";
+        } else {
+            echo "Błąd rejestracji użytkownika";
+        }
+
+    */
 
 
     if($user->login()) {
