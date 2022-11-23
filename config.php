ef98f6b028e2578fe233efd79d27fb8f4c199788
KruZ73 <kruzyckikacper4@gmail.com>
2022-11-23 10:28:21 +0100

    v

--- config.php
+++ config.php
@@ -4,5 +4,5 @@
     $loader = new \Twig\Loader\ArrayLoader([
         'index' => "Hello {{name}}!"
     ]);
-    $twig = new \Twig\Enviroment($loader);
+    $twig = new \Twig\Environment($loader);
 ?>
